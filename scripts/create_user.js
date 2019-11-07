e753811fcf04dd6a6335b382c803f5da185454ff
Scott Reid <reidsm100@gmail.com>
2019-11-07 10:44:34 -0800

    database now contains users

---
+++ scripts/create_user.js
@@ -0,0 +1,14 @@
+function createUser(){
+
+    // if the user is authenticated, get this "user" object
+    // create this user node(doc) in the datebase users collection
+
+    firebase.auth().onAuthStateChanged(function(user){
+        db.collection("users").doc(user.uid).set(
+		{
+        "name":user.displayName, 
+         "email":user.email,
+        });
+    });
+
+}
