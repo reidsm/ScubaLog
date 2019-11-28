f66a23a0dc14ff476492b9dec0a8253465046cad
daniilmol <daniilmik@gmail.com>
2019-11-28 10:59:56 -0800

    Added comments

--- scripts/create_log.js
+++ scripts/create_log.js
@@ -1,3 +1,4 @@
+//this method creates the log once a user submits their logs via submit button
 function createLog(logObject){
 
     // if the user is authenticated, get this "user" object
@@ -12,32 +13,4 @@ function createLog(logObject){
             console.log("there was an error");
         });
     });
-    
-    // var promise = new Promise(function(resolve, reject) {
-    //     // do a thing, possibly async, then…
-      
-    //     firebase.auth().onAuthStateChanged(function(user){
-    //         //add the log object to the user's collection of logs.
-    //         db.collection("users").doc(user.uid).collection("logs").add(logObject);
-    //     });
-
-    //     if (/* everything turned out fine */) {
-    //       resolve("Stuff worked!");
-    //     }
-    //     else {
-    //       reject(Error("It broke"));
-    //     }
-    //   });
-
-    //   promise.then(function(result) {
-    //     window.location.href="index.html";
-    //     console.log(result); // "Stuff worked!"
-    //   }, function(err) {
-    //     console.log(err); // Error: "It broke"
-    //   });
-
-
 }
-
-//.then(function() {
-    // window.location.href="index.html";
