161bdface441d57cfa309846b768ec1d5d3f32d4
daniilmol <daniilmik@gmail.com>
2019-12-03 12:18:01 -0800

    I don't know what these are but I have to push them

--- scripts/logScreenHandleSubmit.js
+++ scripts/logScreenHandleSubmit.js
@@ -1,21 +1,7 @@
 //This script writes the log to the database
-<<<<<<< HEAD
-function submitform(){
-    var f = document.getElementsByTagName('form')[0];
-    if(f.checkValidity()){
-        f.submit();
-        handleSubmit();
-    }else{
-        alert(document.getElementById('example').validationMessage);
-    }
-}
-function handleSubmit(){
-    console.log("HAHA Funny");
-=======
 
 function handleSubmit() {
     
->>>>>>> 3a03c54ab6fee3704fe21edd443e3498e4cf5265
     //gather up the data fields
     var diveSite = document.getElementById('diveSite').value;
     var location = document.getElementById('location').value;
